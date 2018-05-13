de39a8fd8cb8cfeb874d2d91a3549b4f72977c33
soumyaRauth <shoummorauth@live.co.uk>
2018-05-14 00:18:46 +0600

    change input box focus on number length 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,19 @@ import { Keyboard } from '@ionic-native/keyboard';
 })
 export class HomePage implements OnInit{
   @ViewChild('setCursor') cursorSet ;
+  @ViewChild('setCursorMinute') cursorSetMin ;
   constructor(public navCtrl: NavController,private keyboard: Keyboard) {
     
+  }
+
+  onInputTime(e){
+    console.log(e.length);
+    if(e.length===2){
+      setTimeout(() => {
+        this.cursorSetMin.setFocus();
+      },150);
+    }
+
   }
 
   /**sets the cursor to the input after 150 ms after the app opens */
