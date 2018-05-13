4cacf3b26469b3f8911b75fbb5e749a380722dcb
soumyaRauth <shoummorauth@live.co.uk>
2018-05-13 19:44:54 +0600

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-
+export class HomePage implements OnInit{
+  @ViewChild('setCursor') cursorSet ;
   constructor(public navCtrl: NavController) {
-
+    
   }
+
+  /**sets the cursor to the input after 150 ms after the app opens */
+  ngOnInit() {
+    setTimeout(() => {
+      this.cursorSet.setFocus();
+    },150);
+
+ }
 
 }
